8731569d7fb86cd6812a6f7ae9dc101ec5736795
plearner <953413537@qq.com>
2017-04-22 18:08:23 +0800

    fix value to defaultValue in component TodoInput

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
   constructor(props){
     super(props)
     this.state = {
-      newTodo: "test",
+      newTodo: "",
       todoList: [
         {id: 1, title: "第一个代办"},
         {id: 2, title: "第二个代办"}

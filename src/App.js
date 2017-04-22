545a54717c60b11e7c908a7daf17afdfae34e329
plearner <953413537@qq.com>
2017-04-22 23:01:04 +0800

    新增todo

--- src/App.js
+++ src/App.js
@@ -12,25 +12,24 @@ class App extends Component {
     this.state = {
       newTodo: "",
       todoList: [
-        {id: 1, title: "第一个代办"},
-        {id: 2, title: "第二个代办"}
+
       ]
     }
   }
   render() {
     let todos = this.state.todoList.map((item,index)=>{
       return (
-        <li>
+        <li key={index} >
           <TodoItem todo={item} />
         </li>
       )
     })
-
+    console.log(todos)
     return (
       <div className="App">
         <h1>我的代办</h1>
         <div className="inputWrapper">
-          <TodoInput content={this.state.newTodo} onSubmit={this.addTodo} />
+          <TodoInput content={this.state.newTodo} onSubmit={this.addTodo.bind(this) } />
         </div>
         <ol>
           {todos}
@@ -38,9 +37,25 @@ class App extends Component {
       </div>
     );
   }
-  addTodo(){
-    console.log('我的添加一个todo了啊')
+  addTodo(event){
+    this.state.todoList.push({
+      id: idMaker(),
+      title: event.target.value,
+      status: null,
+      deleted: false
+    })
+    this.setState({
+      newTodo: '',
+      todoList: this.state.todoList
+    })
   }
 }
 
 export default App;
+
+let id = 0
+
+function idMaker(){
+  id += 1
+  return id
+}

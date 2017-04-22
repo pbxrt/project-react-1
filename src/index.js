d87e395cf9afad0df99adf4aaf5c3e8f00071a0a
plearner <953413537@qq.com>
2017-04-22 16:55:08 +0800

    基本骨架完成

--- src/index.js
+++ src/index.js
@@ -3,7 +3,43 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+/*
+function tick(){
+	const element = (
+		<div>
+			<h1>Hello, world!</h1>
+			<h2>It is {new Date().toLocaleTimeString()}.</h2>
+		</div>
+	);
+	ReactDOM.render(
+		element,
+		document.getElementById('root')
+	)
+}
+
+setInterval(tick,1000)
+*/
+
+/*
+第二次练习，练习组件的写法
+class Welcome extends React.Component{
+	render(){
+		return <h1>Hello, Component</h1>;
+	}
+}
+
 ReactDOM.render(
-  <h1>Hello,Frank,I made it!</h1>,
-  document.getElementById('root2')
-);
+	<Welcome/>,
+	document.getElementById('root')
+)
+
+*/
+
+
+
+// 第二次练习，练习组件的写法
+
+
+
+
+

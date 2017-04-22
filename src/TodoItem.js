b818568b8ff4e37712a1904311d332fd8aa35f37
plearner <953413537@qq.com>
2017-04-22 23:29:20 +0800

    删除todo

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -2,6 +2,18 @@ import React, {Component} from 'react';
 
 export default class TodoItem extends Component {
 	render(){
-		return <div>{this.props.todo.title}</div>
+		return (
+			<div>
+				<input type="checkbox" checked={this.props.todo.status === 'completed'}
+				onChange={this.toggle.bind(this)} /> {this.props.todo.title}
+				<button onClick={this.delete.bind(this)} >删除</button>
+			</div>
+		)
+	}
+	toggle(e){
+		this.props.onToggle(e,this.props.todo)
+	}
+	delete(e){
+		this.props.onDelete(e,this.props.todo)
 	}
 }

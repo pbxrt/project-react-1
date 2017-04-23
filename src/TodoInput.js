36cb31a033553f18de5e57eb922b3abf00e0a557
plearner <953413537@qq.com>
2017-04-23 16:09:50 +0800

    修改TodoInput,添加事件参数

--- src/TodoInput.js
+++ src/TodoInput.js
@@ -2,14 +2,17 @@ import React ,{Component} from 'react';
 import './TodoInput.css'
 export default class TodoInput extends Component {
 	render(){
-		return <input type="text" defaultValue={this.props.content}
-			className="TodoInput"
+		return <input type="text" value={this.props.content}
+			onChange = {this.changeTitle.bind(this)}
 			onKeyPress={this.submit.bind(this)} />
 	}
 	submit(e){
 		if(e.key === 'Enter') {
 			console.log('用户按回车了');
-			this.props.onSubmit.call()
+			this.props.onSubmit(e)
 		}
+	}
+	changeTitle(e){
+		this.props.onChange(e)
 	}
 }

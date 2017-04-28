d6af8e8f24bf40aa0d20e87518af8cb1950c4ae0
plearner <953413537@qq.com>
2017-04-28 19:20:42 +0800

    install leancloud

--- src/App.js
+++ src/App.js
@@ -7,6 +7,14 @@ import 'normalize.css'
 import './reset.css'
 import * as localStore from './localStore'
 
+
+var APP_ID = 'virPGES2U55XPaxq34P4lClm-gzGzoHsz';
+var APP_KEY = 'vBD4vnwzobFq2JMF9g2Nd61q';
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+});
+
 class App extends Component {
   constructor(props){
     super(props)

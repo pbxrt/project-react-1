d898f098ea98d7e39b2c3d79dfebf33a911935c6
plearner <953413537@qq.com>
2017-04-29 14:11:22 +0800

    将changeUserName和changePassword优化成一个函数changeFormData

--- src/UserDialog.js
+++ src/UserDialog.js
@@ -18,14 +18,9 @@ export default class UserDialog extends Component{
 	}
 	signUp(e){}
 	signIn(e){}
-	changeUsername(e){
+	changeFormData(key,e){
 		let stateCopy = JSON.parse(JSON.stringify(this.state))
-		stateCopy.formData.username = e.target.value
-		this.setState(stateCopy)
-	}
-	changePassword(e){
-		let stateCopy = JSON.parse(JSON.stringify(this.state))
-		stateCopy.formData.password = e.target.value
+		stateCopy.formData[key] = e.target.value
 		this.setState(stateCopy)
 	}
 	render(){
@@ -34,12 +29,12 @@ export default class UserDialog extends Component{
 				<div className="row">
 					<label>用户名</label>
 					<input type="text" value={this.state.formData.username}
-						onChange={this.changeUsername.bind(this)} />
+						onChange={this.changeFormData.bind(this,"username")} />
 				</div>
 				<div className="row">
 					<label>密码</label>
 					<input type="password" value={this.state.formData.password}
-						onChange={this.changePassword.bind(this)} />
+						onChange={this.changeFormData.bind(this,"password")} />
 				</div>
 				<div className="row actions">
 					<button type="submit">注册</button>
@@ -51,12 +46,12 @@ export default class UserDialog extends Component{
 				<div className="row">
 					<label>用户名</label>
 					<input type="text" value={this.state.formData.username}
-						onChange={this.changeUsername.bind(this)} />
+						onChange={this.changeFormData.bind(this,"username")} />
 				</div>
 				<div className="row">
 					<label>密码</label>
 					<input type="password" value={this.state.formData.password}
-						onChange={this.changePassword.bind(this)} />
+						onChange={this.changeFormData.bind(this,"password")} />
 				</div>
 				<div className="row actions">
 					<button type="submit">登录</button>

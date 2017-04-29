1ec031c148bd0f35b8b374bea0586fe7c785c4b0
plearner <953413537@qq.com>
2017-04-29 10:28:48 +0800

    新建leanCloud.js

--- src/App.js
+++ src/App.js
@@ -7,15 +7,6 @@ import 'normalize.css'
 import './reset.css'
 import * as localStore from './localStore'
 
-import AV from 'leancloud-storage'
-var APP_ID = 'virPGES2U55XPaxq34P4lClm-gzGzoHsz';
-var APP_KEY = 'vBD4vnwzobFq2JMF9g2Nd61q';
-AV.init({
-  appId: APP_ID,
-  appKey: APP_KEY
-});
-
-
 
 class App extends Component {
   constructor(props){

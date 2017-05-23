acfe65c73a8636e25de34728e02c6780982927e6
plearner <953413537@qq.com>
2017-05-23 11:38:21 +0800

    将TodoInput组件重构成函数式组件

--- task-5/src/TodoInput.js
+++ task-5/src/TodoInput.js
@@ -1,20 +1,20 @@
-import React ,{Component} from 'react';
+import React from 'react';
 import './TodoInput.css'
-export default class TodoInput extends Component {
-	render(){
-		return <input type="text" placeholder="+ 输入新的待办事项，按Enter添加" 
-			value={this.props.content}
+
+function changeTitle(props,e){
+	props.onChange(e);
+}
+function submit(props,e){
+	if(e.key === 'Enter') {
+		props.onSubmit(e)
+	}
+}
+export default function(props){
+	return (
+		<input type="text" placeholder="+ 输入新的待办事项，按Enter添加" 
+			value={props.content}
 		    className="TodoInput"
-			onChange={this.changeTitle.bind(this)}
-			onKeyPress={this.submit.bind(this)} />
-	}
-	submit(e){
-		if(e.key === 'Enter') {
-			this.props.onSubmit(e)
-			
-		}
-	}
-	changeTitle(e){
-		this.props.onChange(e)
-	}
+			onChange={changeTitle.bind(null,props)}
+			onKeyPress={submit.bind(null,props)} />
+	)
 }
